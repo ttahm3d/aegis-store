cd7c1c951c9f485a8133da391c0fb054401de9cf
ttahm3d <ttahm3d@gmail.com>
2022-03-19 13:14:09 +0530

    feat: made header responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,56 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { AiOutlineMenu } from "react-icons/ai";
 import styles from "./Header.module.css";
 import Logo from "../../assets/AegisStore.svg";
+import { useScreenWidth } from "../../hooks/";
 
 export default function () {
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [width] = useScreenWidth();
+
+  const closeSidebar = () => setShowSidebar(false);
+
   return (
-    <header className={`${styles.header} shadow`}>
+    <header className={`${styles.header}`}>
       <div className="main-container">
-        <nav
-          className={`${styles.navbar} flex align-center flex-gap-2 reducegap`}>
+        <nav className={styles.navbar}>
+          <div
+            className={styles.menu}
+            onClick={() => setShowSidebar((s) => !s)}>
+            <AiOutlineMenu />
+          </div>
           <div className={styles.logo}>
             <Link to="/">
               <img src={Logo} alt="logo" />
             </Link>
           </div>
-          <div className="nav-items flex align-center flex-gap-1">
-            <ul className="nav-list flex align-center style-none flex-gap-1">
+          <div className={styles.navitems}>
+            <ul
+              className={styles.navlist}
+              style={{
+                transform:
+                  showSidebar || width >= 560
+                    ? "translateX(0%)"
+                    : "translateX(-100%)",
+              }}>
               <li className="nav-list-item">
-                <Link to="/wishlist">Wishlist</Link>
+                <Link to="/wishlist" onClick={() => closeSidebar()}>
+                  Wishlist
+                </Link>
               </li>
               <li className="nav-list-item">
-                <Link to="/cart">Cart</Link>
+                <Link to="/cart" onClick={() => closeSidebar()}>
+                  Cart
+                </Link>
               </li>
               <li className="nav-list-item">
-                <a className="btn btn-accent" href="login.html">
+                <Link
+                  className="btn btn-accent"
+                  to="/login"
+                  onClick={() => closeSidebar()}>
                   Login
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
