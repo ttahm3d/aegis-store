f2671d1054452a519e8eb2d125902dfc43cd5e50
ttahm3d <ttahm3d@gmail.com>
2022-03-20 12:27:33 +0530

    feat: created reusable wishlist button

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import WishlistButton from "./WishlistButton/WishlistButton";
 
-export { Header, Footer };
+export { Header, Footer, WishlistButton };
