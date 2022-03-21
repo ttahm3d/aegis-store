2365b89065f0e3fead8c0139e4d8e538b4701824
ttahm3d <ttahm3d@gmail.com>
2022-03-21 15:18:59 +0530

    chore: removed commented code

--- src/pages/products/Listing/Listing.jsx
+++ src/pages/products/Listing/Listing.jsx
@@ -6,21 +6,6 @@ import Filters from "./Filters";
 import Product from "./Product";
 
 export default function () {
-  // const [products, setProducts] = useState([]);
-
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     try {
-  //       const { data } = await axios.get("/api/products");
-  //       setProducts(data?.products);
-  //     } catch (e) {
-  //       console.log("error", e);
-  //     }
-  //   }
-
-  //   fetchData();
-  // }, []);
-
   const { products } = useProducts();
 
   return (
