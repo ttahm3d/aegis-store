d9c12aecc7d50740e0280675f107be569ed35b6d
ttahm3d <ttahm3d@gmail.com>
2022-03-21 18:28:18 +0530

    style- changed style of rating component

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -2,8 +2,27 @@ import styles from "./Rating.module.css";
 import { AiFillStar } from "react-icons/ai";
 
 export default function ({ rating }) {
+  const ratingColor = () => {
+    if (rating >= 2 && rating < 3) {
+      return {
+        backgroundColor: "hsla(var(--red-500), 0.7)",
+        color: "var(--pure-white)",
+      };
+    } else if (rating >= 3 && rating < 4) {
+      return {
+        backgroundColor: "hsl(var(--accent-light))",
+        color: "var(--pure-black)",
+      };
+    } else if (rating >= 4) {
+      return {
+        backgroundColor: "hsl(var(--green-500))",
+        color: "var(--pure-white)",
+      };
+    }
+  };
+
   return (
-    <div className={styles.rating}>
+    <div className={styles.rating} style={ratingColor()}>
       {rating}&nbsp;
       <AiFillStar />
     </div>
