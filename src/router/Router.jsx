6b2cc1401ea8a9515f69fb821b7cb3795c082840
ttahm3d <ttahm3d@gmail.com>
2022-03-26 23:41:20 +0530

    feat: added Outlet for protected routes

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -6,15 +6,18 @@ import { NotFound } from "../pages/utils";
 import { LandingPage } from "../pages/landing-page";
 import { MockApi } from "../pages/api-response";
 import { Signup, Login } from "../pages/auth";
+import RequireAuth from "./RequireAuth";
 
 export default function () {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/wishlist" element={<Wishlist />} />
-      <Route path="/products" element={<Listing />} />
+      <Route element={<RequireAuth />}>
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+      </Route>
       <Route path="/auth/login" element={<Login />} />
+      <Route path="/products" element={<Listing />} />
       <Route path="/auth/signup" element={<Signup />} />
       <Route path="/api-response" element={<MockApi />} />
       <Route path="*" element={<NotFound />} />
