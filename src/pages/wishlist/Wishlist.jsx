110710bf14ea030be027a3b0a933d3bb04e903e0
ttahm3d <ttahm3d@gmail.com>
2022-03-27 18:50:29 +0530

    feat: now displaying wishlisted items

--- src/pages/wishlist/Wishlist.jsx
+++ src/pages/wishlist/Wishlist.jsx
@@ -1,11 +1,30 @@
 import { useWishlist } from "../../context/wishlist";
+import WishlistCard from "./WishlistCard";
+import styles from "./Wishlist.module.css";
+import Empty from "../../components/Empty/Empty";
 
 export default function () {
   const { wishlist } = useWishlist();
 
   return (
     <div className="main-container">
-      {wishlist.length === 0 ? "no items to show" : "wishlist cards"}
+      <div className={styles.wishlist__container}>
+        {wishlist.length === 0 ? (
+          <Empty
+            title={"Your Wishlist is empty"}
+            message="Checkout the products and wishlist the ones you want !!"
+          />
+        ) : (
+          <div className={styles.wishlist__cards}>
+            {wishlist.map((wishlistItem) => (
+              <WishlistCard
+                key={wishlistItem._id}
+                wishlistItem={wishlistItem}
+              />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
