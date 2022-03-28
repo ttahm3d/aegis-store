f08eec93769624495f111a7fbdd68b6e3fe24419
ttahm3d <ttahm3d@gmail.com>
2022-03-28 19:39:58 +0530

    feat: move to wishlist functionality

--- src/pages/wishlist/WishlistCard.jsx
+++ src/pages/wishlist/WishlistCard.jsx
@@ -1,10 +1,13 @@
 import styles from "./Wishlist.module.css";
 import { WishlistButton } from "../../components";
 import { useWishlist } from "../../context/wishlist";
+import { useCart } from "../../context/cart";
 
 export default function ({ wishlistItem }) {
   const { title, name, sellingPrice, imageUrl } = wishlistItem;
   const { removeFromWishlist } = useWishlist();
+
+  const { addToCart } = useCart();
 
   return (
     <div className={`card shadow ${styles.wishlist__card}`}>
@@ -15,7 +18,11 @@ export default function ({ wishlistItem }) {
         <div className={`${styles.wishlist__name}`}>{name}</div>
         <div className={`${styles.wishlist__price}`}>&#8377;{sellingPrice}</div>
         <div className={`${styles.wishlist__buttons__container}`}>
-          <button className="btn btn-primary">Move to Cart</button>
+          <button
+            className="btn btn-primary"
+            onClick={() => addToCart(wishlistItem)}>
+            Move to Cart
+          </button>
           <button
             className="btn btn-seconday"
             onClick={() => removeFromWishlist(wishlistItem)}>
