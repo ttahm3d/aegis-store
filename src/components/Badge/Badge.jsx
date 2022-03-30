8c9bd08bdf7e16278ef7a9fa0613e93a5e6c6180
ttahm3d <ttahm3d@gmail.com>
2022-03-30 19:21:55 +0530

    feat: displaying header values only when logged in

--- src/components/Badge/Badge.jsx
+++ src/components/Badge/Badge.jsx
@@ -1,13 +1,21 @@
+import { useAuth } from "../../context/auth";
 import styles from "./Badge.module.css";
 
 export default function ({ icon, text, number }) {
+  const { isLoggedIn } = useAuth();
+
   return (
     <div className={styles.badge}>
       <div className={styles.badge__icon}>{icon}</div>
       <div className={styles.badge__text}>{text}</div>
-      {number !== 0 && number !== null && (
-        <div className={styles.badge__number}>{number > 9 ? "9+" : number}</div>
-      )}
+      {isLoggedIn
+        ? number !== 0 &&
+          number !== null && (
+            <div className={styles.badge__number}>
+              {number > 9 ? "9+" : number}
+            </div>
+          )
+        : null}
     </div>
   );
 }
