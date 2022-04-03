b69a895931cb7a6c7be1ee38af026e68b446df8e
ttahm3d <ttahm3d@gmail.com>
2022-04-03 17:27:36 +0530

    feat: decresed toast time

--- src/components/Toast/Toast.jsx
+++ src/components/Toast/Toast.jsx
@@ -4,7 +4,7 @@ import "react-toastify/dist/ReactToastify.css";
 export default function ({ message, type }) {
   return toast(message, {
     position: toast.POSITION.BOTTOM_RIGHT,
-    autoClose: 5000,
+    autoClose: 1000,
     type: type,
     transition: Slide,
     closeOnClick: true,
