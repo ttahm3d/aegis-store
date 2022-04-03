ce1300e41528a35d7c25cf9d46a5bdb4ca8daa93
ttahm3d <ttahm3d@gmail.com>
2022-04-03 20:08:23 +0530

    style - fixed some css issues

--- src/pages/products/Listing/Listing.jsx
+++ src/pages/products/Listing/Listing.jsx
@@ -12,18 +12,20 @@ export default function () {
   return (
     <section className={styles.listing__container}>
       <div className="main-container">
-        <div className={styles.products__info}>
-          Showing {products.length && products.length} products of 20
-        </div>
         <div className={`${styles.listing}`}>
           <Filters />
-          <>
+          <div>
+            <>
+              <div className={styles.listing__info}>
+                Showing {products.length && products.length} products of 20
+              </div>
+            </>
             <div className={`${styles.products__container}`}>
               {products?.map((product) => (
                 <Product key={product._id} product={product} />
               ))}
             </div>
-          </>
+          </div>
         </div>
       </div>
     </section>
