49c43c8c4e1712b5078256f3882ba014729f4fce
ttahm3d <ttahm3d@gmail.com>
2022-06-18 22:02:00 +0530

    chore: created constants for cart

---
+++ src/context/cart/utils/constants.jsx
@@ -0,0 +1,6 @@
+export const GET_CART_ITEMS = "GET_CART_ITEMS";
+export const ADD_TO_CART = "ADD_TO_CART";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+export const INCREASE_QUANTITY = "INCREASE_QUANTITY";
+export const DECREASE_QUANTITY = "DECREASE_QUANTITY";
+export const RESET = "RESET";
