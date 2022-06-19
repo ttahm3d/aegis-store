1c43046c33eefad0b4177670b4602382cdb006f1
ttahm3d <ttahm3d@gmail.com>
2022-06-19 17:45:00 +0530

    feat: refactored wishlist

--- src/context/wishlist/Reducer.jsx
+++ src/context/wishlist/Reducer.jsx
@@ -1,8 +1,14 @@
+import {
+  SET_WISHLIST,
+  ADD_TO_WISHLIST,
+  REMOVE_FROM_WISHLIST,
+} from "./utils/constants";
+
 const wishlistReducer = (state, { type, payload }) => {
   switch (type) {
-    case "SET_WISHLIST":
-    case "ADD_TO_WISHLIST":
-    case "REMOVE_FROM_WISHLIST":
+    case SET_WISHLIST:
+    case ADD_TO_WISHLIST:
+    case REMOVE_FROM_WISHLIST:
       return {
         ...state,
         wishlist: payload,
